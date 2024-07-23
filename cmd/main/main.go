a7491c5eee39def32935ec3f16f358f7629f6612
Paluan Batista <48101122+paluan-batista@users.noreply.github.com>
2024-07-23 09:41:08 -0300

    Update main.go
    removendo init swagger

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,11 +2,6 @@ package main
 
 import "go-driver-register/internal/server"
 
-// @title Go Driver Register API
-// @version 1.0
-// @description API for managing drivers and vehicles
-// @host localhost:8000
-// @basePath /
 func main() {
 	server.StartServer()
 }
